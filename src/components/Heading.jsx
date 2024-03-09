a15888d9f9789774aceeebc6b1b7726b24025a11
DarumaDaichi <sudarmoadi89@gmail.com>
2024-03-09 18:52:37 +0700

    finishing benfits section

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,7 +2,7 @@
 
 const Heading = ({className, title}) => {
     return (
-        <div className={`${className} max-w-[50rem] mb-12 lg:mb-20`}>
+        <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}>
             {title && <h2 className="h2">{title}</h2>}
         </div>
     )
